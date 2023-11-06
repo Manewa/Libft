ae26f6c030b5f43afbac0ffd6057560d1d5ee81b
Nathan Malier <namalier@paul-f3Ar1s2.clusters.42paris.fr>
2023-11-06 17:31:14 +0100

    Header x Day 1

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,36 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: namalier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/06 16:54:26 by namalier          #+#    #+#             */
-/*   Updated: 2023/11/06 16:54:28 by namalier         ###   ########.fr       */
+/*   Created: 2023/11/06 17:12:42 by namalier          #+#    #+#             */
+/*   Updated: 2023/11/06 17:30:46 by namalier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"libft.h"
-
-char *ft_strnstr(const char *str, const char *tofind, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t i;
-	size_t j;
-	i = 0;
-
-	if (!tofind[i])
-			return ((char *)str);
-	while(i < n && str[i])
-	{
-		if (str[i] == tofind[0])
-		{
-			j = 0;
-			while (tofind[j] && str[i + j] == tofind[j] && i + j < n)
-					j++;
-			if (!tofind[j])
-				return ((char *)&str[i]);
-		}
-		i++;
-	}
-	return (0);
+	
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -1,36 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: namalier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/06 16:54:26 by namalier          #+#    #+#             */
-/*   Updated: 2023/11/06 16:54:28 by namalier         ###   ########.fr       */
+/*   Created: 2023/11/06 15:59:40 by namalier          #+#    #+#             */
+/*   Updated: 2023/11/06 17:09:26 by namalier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"libft.h"
+#include "libft.h"
 
-char *ft_strnstr(const char *str, const char *tofind, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t i;
-	size_t j;
-	i = 0;
-
-	if (!tofind[i])
-			return ((char *)str);
-	while(i < n && str[i])
-	{
-		if (str[i] == tofind[0])
-		{
-			j = 0;
-			while (tofind[j] && str[i + j] == tofind[j] && i + j < n)
-					j++;
-			if (!tofind[j])
-				return ((char *)&str[i]);
-		}
-		i++;
-	}
-	return (0);
+	char tmp[n];
+	
+	ft_memcpy(tmp, src, n);
+	ft_memcpy(dest, tmp, n);
+	return (dest);
 }
